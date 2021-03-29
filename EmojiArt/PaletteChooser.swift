cce037bb08f09210161e77150138ffd2b5e1e267
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-29 21:09:26 +0300

    Add popover PaletteEditor

--- EmojiArt/PaletteChooser.swift
+++ EmojiArt/PaletteChooser.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct PaletteChooser: View {
     @ObservedObject var document: EmojiArtDocument
     @Binding var chosenPalette: String
+    @State private var showPaletteEditor: Bool = false
     
     var body: some View {
         HStack {
@@ -24,8 +25,67 @@ struct PaletteChooser: View {
                     EmptyView()
                 })
             Text(document.paletteNames[chosenPalette] ?? "")
+            Image(systemName: "keyboard").imageScale(.large)
+                .onTapGesture {
+                    showPaletteEditor = true
+                }
+                .popover(isPresented: $showPaletteEditor) {
+                    PaletteEditor(chosenPalette: $chosenPalette)
+                        .environmentObject(document)
+                        .frame(minWidth: 300, minHeight: 500)
+                }
         }
         .fixedSize(horizontal: true, vertical: false)
+    }
+}
+
+struct PaletteEditor: View {
+    @EnvironmentObject var document: EmojiArtDocument
+    @Binding var chosenPalette: String
+    @State private var paletteName: String = ""
+    @State private var emojisToAdd: String = ""
+    
+    // MARK: - Drawing Constants
+    let columnsCount: Int = 5
+    let fontSize: CGFloat = 40
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Text("Palette Editor")
+                .font(.headline)
+                .padding()
+            Divider()
+            Form {
+                Section {
+                    TextField("Palette Name", text: $paletteName) { began in
+                        if !began {
+                            document.renamePalette(chosenPalette, to: paletteName)
+                        }
+                    }
+                    TextField("Add Emoji", text: $emojisToAdd) { began in
+                        if !began {
+                            chosenPalette = document.addEmoji(emojisToAdd, toPalette: chosenPalette)
+                            emojisToAdd = ""
+                        }
+                    }
+                }
+                Section(header: Text("Remove Emoji")) {
+                    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: columnsCount)
+                    LazyVGrid(columns: columns) {
+                        ForEach(chosenPalette.map { String($0) }, id: \.self) { emoji in
+                            Text(emoji)
+                                .font(Font.system(size: fontSize))
+                                .onTapGesture {
+                                    chosenPalette = document.removeEmoji(emoji, fromPalette: chosenPalette)
+                                }
+                        }
+                    }
+                }
+            }
+        }
+        .onAppear {
+            paletteName = document.paletteNames[chosenPalette] ?? ""
+        }
     }
 }
 
